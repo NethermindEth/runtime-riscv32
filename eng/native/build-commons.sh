5528fcd153009464655bbda60c70ed846133b11c
Maxim Menshikov <maxim@menshikov.org>
2025-02-21 01:55:28 +0000

    Engine: native build for riscv32

--- eng/native/build-commons.sh
+++ eng/native/build-commons.sh
@@ -251,7 +251,7 @@ usage()
     echo ""
     echo "Common Options:"
     echo ""
-    echo "BuildArch can be: -arm, -armv6, -armel, -arm64, -loongarch64, -riscv64, -s390x, -ppc64le, x64, x86, -wasm"
+    echo "BuildArch can be: -arm, -armv6, -armel, -arm64, -loongarch64, -riscv32, -riscv64, -s390x, -ppc64le, x64, x86, -wasm"
     echo "BuildType can be: -debug, -checked, -release"
     echo "-os: target OS (defaults to running OS)"
     echo "-bindir: output directory (defaults to $__ProjectRoot/artifacts)"
@@ -444,6 +444,10 @@ while :; do
 
         loongarch64|-loongarch64)
             __TargetArch=loongarch64
+            ;;
+
+        riscv32|-riscv32)
+            __TargetArch=riscv32
             ;;
 
         riscv64|-riscv64)
