46bfb96867ce6cbaef89fd35a732dede68434398
Maxim Menshikov <maxim@menshikov.org>
2025-02-27 23:10:13 +0000

    perfjitdump: support riscv32

--- src/coreclr/pal/src/misc/perfjitdump.cpp
+++ src/coreclr/pal/src/misc/perfjitdump.cpp
@@ -53,6 +53,8 @@ namespace
         ELF_MACHINE = EM_AARCH64,
 #elif defined(HOST_LOONGARCH64)
         ELF_MACHINE = EM_LOONGARCH,
+#elif defined(HOST_RISCV32)
+        ELF_MACHINE = EM_RISCV,
 #elif defined(HOST_RISCV64)
         ELF_MACHINE = EM_RISCV,
 #elif defined(HOST_S390X)
