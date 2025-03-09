79f288fe68be86d80d778447b8646b4cdd464d04
Maxim Menshikov <maxim@menshikov.org>
2025-03-09 17:55:48 +0000

    AsmConstants: fix ElementType

--- src/coreclr/vm/riscv32/asmconstants.h
+++ src/coreclr/vm/riscv32/asmconstants.h
@@ -110,7 +110,7 @@ ASMCONSTANTS_C_ASSERT(MethodTable__m_dwFlags == offsetof(MethodTable, m_dwFlags)
 #define MethodTable__m_BaseSize         0x04
 ASMCONSTANTS_C_ASSERT(MethodTable__m_BaseSize == offsetof(MethodTable, m_BaseSize));
 
-#define MethodTable__m_ElementType     DBG_FRE(0x24, 0x30)
+#define MethodTable__m_ElementType     DBG_FRE(0x24, 0x20)
 ASMCONSTANTS_C_ASSERT(MethodTable__m_ElementType == offsetof(MethodTable, m_ElementTypeHnd));
 
 #define ArrayBase__m_NumComponents     0x4
